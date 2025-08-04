d190a24e04e32f0d6c3079ba858c1228a4509c70
Rit0106 <ritikaraj6202088782@gmail.com>
2025-08-05 00:57:56 +0530

    Time: 2 ms (12.45%), Space: 8.3 MB (37.9%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -3,13 +3,17 @@ public:
     bool rotateString(string s, string goal) {
         int n=s.size();
         int m=goal.size();
-        if(n!=m) return false;
+       /* if(n!=m) return false;
         
         for(int i=0;i<n;i++){
             s+=s[i];
             string sub=s.substr(i+1,n+1);
             if(sub==goal) return true;
 
+        }
+        return false; */
+        if(n==m && (s+s).find(goal)<s.size()){
+            return true;
         }
         return false;
         
