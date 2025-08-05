9c7101f77eb33098c1f3e810d2619c4d232a1eeb
Rit0106 <ritikaraj6202088782@gmail.com>
2025-08-06 03:27:22 +0530

    Time: 3 ms (42.17%), Space: 11.1 MB (38.92%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        int n = s.length();
+        int i = 0;
+
+        // Step 1: Extract words ignoring extra spaces
+        while (i < n) {
+            while (i < n && s[i] == ' ') i++; // Skip spaces
+            if (i >= n) break;
+            int j = i;
+            while (j < n && s[j] != ' ') j++; // Find word
+            words.push_back(s.substr(i, j - i));
+            i = j;
+        }
+
+        // Step 2: Reverse the vector of words
+        reverse(words.begin(), words.end());
+
+        // Step 3: Join them with single space
+        string result;
+        for (int k = 0; k < words.size(); k++) {
+            result += words[k];
+            if (k != words.size() - 1)
+                result += ' ';
+        }
+
+        return result;
+    }
+};
