197a73efa750078f2c0441dcf102062be070c331
Rit0106 <ritikaraj6202088782@gmail.com>
2025-10-22 02:58:08 +0530

    Time: 16 ms (61.14%), Space: 25.9 MB (55.41%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& words) {  unordered_map<string, vector<string>> anagramGroups;
+
+        for (string word : words) {
+            string sortedWord = word;
+            sort(sortedWord.begin(), sortedWord.end());
+            anagramGroups[sortedWord].push_back(word);
+        }
+
+        vector<vector<string>> result;
+        for (auto group : anagramGroups) {
+            result.push_back(group.second);
+        }
+
+        return result;
+    }
+        
+    
+};
