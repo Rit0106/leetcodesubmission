dba0b4f3b8f89fc5c3eeba94b10deeba48fd917a
Rit0106 <ritikaraj6202088782@gmail.com>
2025-11-07 01:41:19 +0530

    Time: 0 ms (100%), Space: 9.6 MB (44.68%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastRemaining(int n) {
+         int head = 1;
+        int step = 1;
+        bool lefttoright = true;
+        int remaining = n;
+
+        while (remaining > 1) {
+            if (lefttoright || remaining % 2 == 1) {
+                head += step;
+            }
+            remaining /= 2;
+            step *= 2;
+            lefttoright = !lefttoright;
+        }
+
+        return head;
+        
+    }
+};
