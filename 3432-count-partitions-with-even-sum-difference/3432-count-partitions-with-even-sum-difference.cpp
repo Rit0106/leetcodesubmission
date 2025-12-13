b6e6db523df0067163c94e6c0e8d7771c037fef6
Rit0106 <ritikaraj6202088782@gmail.com>
2025-12-13 23:52:42 +0530

    Time: 0 ms (100%), Space: 22.5 MB (20.4%) - LeetHub

---
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+          int sum = 0;
+        for(int it : nums)
+            sum += it;  
+        int cnt = 0;
+        int temp_sum = 0;
+        for(int i = 0; i < nums.size()-1; i++){
+                temp_sum += nums[i];
+            if(abs((sum-temp_sum)-(temp_sum)) % 2 == 0)
+                cnt += 1;
+        }
+        return cnt;    
+    }
+};
