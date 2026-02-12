4d68bd514f9c50f22bae40754237ee204af53e71
Rit0106 <ritikaraj6202088782@gmail.com>
2026-02-13 00:16:10 +0530

    Time: 0 ms (100%), Space: 193.4 MB (54.36%) - LeetHub

---
+++ 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumPrefixLength(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                continue;
+            }
+            else{
+                return i+1;
+            }
+        }  
+        return 0;      
+    }
+};
