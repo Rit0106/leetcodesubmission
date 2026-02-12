507b7cd50286aaf3e58e59fcc8e6a9f93eb69eb6
Rit0106 <ritikaraj6202088782@gmail.com>
2026-02-13 00:09:34 +0530

    Time: 247 ms (24.49%), Space: 27.2 MB (30.89%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -2,32 +2,25 @@ class Solution {
 public:
     int longestBalanced(string s) {
         unordered_map<char, int> mp;
-        int sz = s.size(), ans = 0;
-        
-        // Try all possible substrings
-        for(int i = 0; i < sz; i++) {
-            mp = {};  // Reset map for each starting position
-            
-            for(int j = i; j < sz; j++) {
+        int n = s.size();
+        int ans = 0;
+        for(int i = 0;i<n;i++) {
+            mp = {};
+            for(int j =i;j<n;j++) {
                 mp[s[j]]++;
-                
-                // Check if current substring is balanced
-                bool flag = true;
-                int cnt = mp[s[j]];  // Get frequency of current character
-                
+                bool flag= true;
+                int cnt=mp[s[j]];  
                 for(auto x : mp) {
-                    if(x.second != cnt) {
-                        flag = false;
+                    if(x.second !=cnt) {
+                        flag= false;
                         break;
                     }
                 }
-                
                 if(flag) {
-                    ans = max(ans, j - i + 1);
+                    ans = max(ans,j-i+1);
                 }
             }
         }
-        
         return ans;
     }
 };
