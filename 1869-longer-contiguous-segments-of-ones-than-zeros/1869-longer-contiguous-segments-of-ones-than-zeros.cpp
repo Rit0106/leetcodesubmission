1128746b625e715b1e3ea7e27b41c7059f17d61b
Rit0106 <ritikaraj6202088782@gmail.com>
2026-03-07 01:54:23 +0530

    Time: 0 ms (100%), Space: 8 MB (96.47%) - LeetHub

---
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkZeroOnes(string s) {
+        int max1=0;
+        int max0=0;
+        int zero=0;
+        int one=0;
+        for(char ch:s){
+            if(ch=='1'){
+                one++;
+                zero=0;
+                max1=max(max1,one);
+
+            }
+            else{
+                zero++;
+                one=0;
+                max0=max(max0, zero);
+            }
+        }
+        return max1>max0 ;
+    }
+};
