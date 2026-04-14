1d99da76d86c772a77fdd838c101a59d90c7747b
RITIKA RAJ <ritikaraj6202088782@gmail.com>
2026-04-14 16:25:26 +0530

    Time: 0 ms (100%), Space: 28.9 MB (75.26%) - LeetHub

---
+++ 3899-angles-of-a-triangle/3899-angles-of-a-triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<double> internalAngles(vector<int>& sides) {
+         double a=sides[0];
+         double b=sides[1], c=sides[2];
+    if (a+b<=c || b+c<=a || a+c<=b)
+        return {};
+    double A=acos((b*b+c*c-a*a)/(2*b*c));
+    double B=acos((a*a+c*c-b*b)/(2*a*c));
+    double C=acos((a*a+b*b- c*c)/(2*a*b));
+    A = A*180.0/M_PI;
+    B = B*180.0/ M_PI;
+    C = C*180.0/M_PI;
+    vector<double> angles ={A,B,C};
+    sort(angles.begin(),angles.end());
+    return angles;
+    }
+};
