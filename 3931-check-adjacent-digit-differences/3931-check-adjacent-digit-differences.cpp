fcf262390176ee0c82fc9128bcf4a021be671edf
RITIKA RAJ <ritikaraj6202088782@gmail.com>
2026-05-21 15:50:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (78.12%) - LeetHub

---
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        int n=s.size();
+        for(int i=0;i<n-1;i++){
+                int diff=abs(s[i+1]-s[i]);
+                if(diff>2)
+                    return false;
+            
+        }
+        return true;
+    }
+};
