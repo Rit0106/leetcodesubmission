0e30653ab6223f789db1746666b1081392763cc4
RITIKA RAJ <ritikaraj6202088782@gmail.com>
2026-06-10 17:58:04 +0530

    Time: 18 ms (55.84%), Space: 17.6 MB (47.66%) - LeetHub

--- 3941-password-strength/3941-password-strength.cpp
+++ 3941-password-strength/3941-password-strength.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int passwordStrength(string password) {
-        unordered_set<char> st(password.begin(), password.end());
-
-        int ans = 0;
-
+        unordered_set<char>st(password.begin(),password.end());
+        int ans=0;
         for(char ch : st) {
             if('a' <= ch && ch <= 'z')
-                ans += 1;
+                ans+=1;
             else if('A' <= ch && ch <= 'Z')
-                ans += 2;
+                ans+=2;
             else if('0' <= ch && ch <= '9')
-                ans += 3;
+                ans+=3;
             else if(ch == '!' || ch == '@' || ch == '#' || ch == '$')
-                ans += 5;
+                ans+=5;
         }
 
         return ans;
